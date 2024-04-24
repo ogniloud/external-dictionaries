0da5e9fc4b74efc86d33d8125acd470865aa70be
MIZINCHIK <mizinchik@yahoo.com>
2024-04-25 02:13:23 +0700

    Mongomodel: Adapt model to new standard

--- mongomodel/src/main/java/com/madr/external_dictionaries/mongomodel/model/Example.java
+++ mongomodel/src/main/java/com/madr/external_dictionaries/mongomodel/model/Example.java
@@ -2,9 +2,11 @@ package com.madr.external_dictionaries.mongomodel.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Example {
     private String text;
     private String ref;
--- mongomodel/src/main/java/com/madr/external_dictionaries/mongomodel/model/Sense.java
+++ mongomodel/src/main/java/com/madr/external_dictionaries/mongomodel/model/Sense.java
@@ -3,9 +3,11 @@ package com.madr.external_dictionaries.mongomodel.model;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Sense {
     private List<Example> examples;
     private List<String> glosses;
