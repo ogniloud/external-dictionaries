78fb6e152947e0b20f2ecf6c052349fa93c9f3ca
MIZINCHIK <mizinchik@yahoo.com>
2024-04-24 18:15:34 +0700

    General: Fixes & build

--- monctionary/README.md
+++ monctionary/README.md
@@ -1,1 +1,1 @@
-### **A Spring app for loading the wiktionary dictionaries into a MongoDB. To be deployed in an container (see packages).**
+### **A Spring Boot app for loading the wiktionary dictionaries into a MongoDB. To be deployed in a container (see packages).**
--- monctionary/src/main/java/com/madr/external_dictionaries/monctionary/repository/WordRepository.java
+++ monctionary/src/main/java/com/madr/external_dictionaries/monctionary/repository/WordRepository.java
@@ -1,6 +1,6 @@
 package com.madr.external_dictionaries.monctionary.repository;
 
-import com.madr.external_dictionaries.monctionary.model.Word;
+import com.madr.external_dictionaries.mongomodel.model.Word;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
