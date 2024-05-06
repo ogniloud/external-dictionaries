2f81038fe2cb6908e324d21377ae9ffd2561f5a3
MIZINCHIK <mizinchik@yahoo.com>
2024-05-06 11:51:57 +0700

    Languages: Add support to Russian & Spanish locales and languages

--- monctionary/src/main/java/com/madr/external_dictionaries/monctionary/Runner.java
+++ monctionary/src/main/java/com/madr/external_dictionaries/monctionary/Runner.java
@@ -22,6 +22,14 @@ public class Runner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
         fillDb(new File("./mongo-entrypoint/en_to_en.json"), Common.SupportedLanguage.EN, Common.SupportedLanguage.EN);
+        fillDb(new File("./mongo-entrypoint/ru_to_en.json"), Common.SupportedLanguage.EN, Common.SupportedLanguage.RU);
+        fillDb(new File("./mongo-entrypoint/es_to_en.json"), Common.SupportedLanguage.EN, Common.SupportedLanguage.ES);
+        fillDb(new File("./mongo-entrypoint/en_to_ru.json"), Common.SupportedLanguage.RU, Common.SupportedLanguage.EN);
+        fillDb(new File("./mongo-entrypoint/ru_to_ru.json"), Common.SupportedLanguage.RU, Common.SupportedLanguage.RU);
+        fillDb(new File("./mongo-entrypoint/es_to_ru.json"), Common.SupportedLanguage.RU, Common.SupportedLanguage.ES);
+        fillDb(new File("./mongo-entrypoint/en_to_es.json"), Common.SupportedLanguage.ES, Common.SupportedLanguage.EN);
+        fillDb(new File("./mongo-entrypoint/ru_to_es.json"), Common.SupportedLanguage.ES, Common.SupportedLanguage.RU);
+        fillDb(new File("./mongo-entrypoint/es_to_es.json"), Common.SupportedLanguage.ES, Common.SupportedLanguage.ES);
         terminateApplication();
     }
 
